627c70cfbc59769d1d3cee19447fb4741d7621d0
Mateusz <debest245@gmail.com>
2019-05-28 10:19:33 +0200

    Dodanie komentarzy do funkcji w pliku request.php

--- classes/request.php
+++ classes/request.php
@@ -6,21 +6,47 @@ class Request {
 	public const POST = 1;
 	public const GET = 2;
 	public const REQUEST = 3;
-
-	public function __construct($val, $type) {
+	
+	/* 
+	 * Construct
+	 * @param array $val 
+	 * @param int $type
+	 */
+	public function __construct($val, $type) { 
 		$this->values = $val;
 		$this->type = $type;
 	}
+	
+	/*
+	 * Function that will return value from global variable by key
+	 * @param string $key
+	 * @param mixed $default
+	 * @return mixed
+	 */
 	public function get($key, $default = null) {
 		if (isset($this->values[$key]) == false) {
 			return $default;
 		}
 		return $this->values[$key];
 	}
+	
+	/*
+	 * Modifies local value under chosen key 
+	 * @param string $key
+	 * @param mixed $value
+	 * @return bool
+	 */
 	public function set($key, $value) {
 		$this->values[$key] = $value;
 		return true;
 	}
+	
+	/*
+	 * Modifies local and global value under chosen key 
+	 * @param string $key
+	 * @param mixed $value
+	 * @return null
+	 */
 	public function setGlobal($key, $values) {
 		$this->set($key, $values);
 		switch($this->type) {
@@ -35,6 +61,12 @@ class Request {
 				break;
 		}
 	}
+	
+	/*
+	 * Checks if chosen key exist in array
+	 * @param string $key
+	 * @return bool if exists - true, else return false
+	 */
 	public function has($key) {
 		if (isset($this->values[$key])) {
 			return true;
@@ -43,9 +75,14 @@ class Request {
 			return false;
 		}
 	}
+	
+	/*
+	 * Function check if value under chosen key is empty
+	 * @param string $key
+	 * @return bool if empty - true, else return false
+	 */
 	public function isEmpty($key) {
 		if (empty($this->values[$key])) {
-			
 			return true;
 		} else {
 			return false;
