0d5db244177a47690cc76bd7b5f9265f82962932
ravorr <bartek.plonka.ti@gmail.com>
2019-11-07 14:40:44 +0100

    Aktualizacja bazy danych Dodanie nowych tabel oraz kluczy

---
+++ install/data.sql
@@ -0,0 +1,30 @@
+INSERT INTO `state` (`id`, `state`) VALUES (1, 'dolnośląskie');
+INSERT INTO `state` (`id`, `state`) VALUES (2, 'kujawsko-pomorskie');
+INSERT INTO `state` (`id`, `state`) VALUES (3, 'lubelskie');
+INSERT INTO `state` (`id`, `state`) VALUES (4, 'lubuskie');
+INSERT INTO `state` (`id`, `state`) VALUES (5, 'łódzkie');
+INSERT INTO `state` (`id`, `state`) VALUES (6, 'małopolskie');
+INSERT INTO `state` (`id`, `state`) VALUES (7, 'mazowieckie');
+INSERT INTO `state` (`id`, `state`) VALUES (8, 'opolskie');
+INSERT INTO `state` (`id`, `state`) VALUES (9, 'podkarpackie');
+INSERT INTO `state` (`id`, `state`) VALUES (10, 'podlaskie');
+INSERT INTO `state` (`id`, `state`) VALUES (11, 'pomorskie');
+INSERT INTO `state` (`id`, `state`) VALUES (12, 'śląskie');
+INSERT INTO `state` (`id`, `state`) VALUES (13, 'świętokrzyskie');
+INSERT INTO `state` (`id`, `state`) VALUES (14, 'warmińsko-mazurskie');
+INSERT INTO `state` (`id`, `state`) VALUES (15, 'wielkopolskie');
+INSERT INTO `state` (`id`, `state`) VALUES (16, 'zachodniopomorskie');
+
+INSERT INTO `type` (`id`, `type`) VALUES (1, 'mieszane');
+INSERT INTO `type` (`id`, `type`) VALUES (2, 'plastik');
+INSERT INTO `type` (`id`, `type`) VALUES (3, 'papier');
+INSERT INTO `type` (`id`, `type`) VALUES (4, 'metale - żelazo');
+INSERT INTO `type` (`id`, `type`) VALUES (5, 'metale - aluminium');
+INSERT INTO `type` (`id`, `type`) VALUES (6, 'metale - miedź');
+INSERT INTO `type` (`id`, `type`) VALUES (7, 'metale - inne');
+
+INSERT INTO `container` (`id`, `container`) VALUES (1, 'worki');
+INSERT INTO `container` (`id`, `container`) VALUES (2, 'kubły');
+
+INSERT INTO `qty_type` (`id`, `type`) VALUES (1, 'litry');
+INSERT INTO `qty_type` (`id`, `type`) VALUES (2, 'kilogramy');
--- install/scheme.sql
+++ install/scheme.sql
@@ -1,22 +1,50 @@
-CREATE TABLE users (
-  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  username VARCHAR(150) NOT NULL,
-  first_name VARCHAR(150) NOT NULL,
-  last_name VARCHAR(150) NOT NULL,
-  password VARCHAR(250) NOT NULL,
-  email VARCHAR(150) NOT NULL,
-  state VARCHAR(250) NOT NULL,
-  county VARCHAR(250) NOT NULL,
-  city VARCHAR(250) NOT NULL,
-  postal_code VARCHAR(250) NOT NULL,
-  street VARCHAR(250) NOT NULL);
+
+CREATE TABLE `state` ( 
+    `id` INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `state` VARCHAR(100) NOT NULL
+) ENGINE=InnoDB;
+
+CREATE TABLE `type` (
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `type` varchar(150)
+) ENGINE=InnoDB;
+
+CREATE TABLE `container` (
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `container` varchar(150)
+) ENGINE=InnoDB;
+
+CREATE TABLE `qty_type` (
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `type` varchar(150)
+) ENGINE=InnoDB;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `username` varchar(150) NOT NULL,
+  `password` varchar(250) NOT NULL,
+  `first_name` varchar(150) NOT NULL,
+  `last_name` varchar(150) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `state_id` int(11) NOT NULL,
+  `county` varchar(250) NOT NULL,
+  `city` varchar(250) NOT NULL,
+  `postal_code` varchar(250) NOT NULL,
+  `street` varchar(250) NOT NULL,
+   FOREIGN KEY (`state_id`)
+        REFERENCES `state`(`id`) ON UPDATE CASCADE ON DELETE CASCADE
+) ENGINE=InnoDB;
 
 CREATE TABLE `dispose` (
-	`id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	`date` DATE, 
-	`type` TINYINT,
-	`container_type` TINYINT,
-	`qty` MEDIUMINT,
-	`quantity_type` TINYINT,
-	`user_id` INT NOT NULL
-)
+  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `type_id` int(11) NOT NULL,
+  `container_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `qty_type_id` int(11) NOT NULL,
+    FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (`type_id`) REFERENCES `type`(`id`) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (`container_id`) REFERENCES `container`(`id`) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (`qty_type_id`) REFERENCES `qty_type`(`id`) ON UPDATE CASCADE ON DELETE CASCADE
+) ENGINE=InnoDB;
